46ddc472a30ade128c8d0663a2900de6da60961c
Mr.Nobody <njunge995@gmail.com>
2025-04-14 18:33:32 +0300

    Hackernews GraphQL Final Touches

--- hackernews-node-ts/src/context.ts
+++ hackernews-node-ts/src/context.ts
@@ -1,3 +1,15 @@
-export async function contextFactory() {
-    return {}
+import { PrismaClient, User } from "@prisma/client"
+import { FastifyRequest } from "fastify";
+import { authenticateUser } from "./auth";
+const prisma = new PrismaClient()
+
+export type GraphQLContext = {
+    prisma: PrismaClient;
+    currentUser: User | null
+}
+export async function contextFactory(request: FastifyRequest):Promise<GraphQLContext> {
+    return {
+        prisma,
+        currentUser: await authenticateUser(prisma, request)
+    }
 }
--- hackernews-node-ts/src/schema.ts
+++ hackernews-node-ts/src/schema.ts
@@ -1,49 +1,100 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import typeDefs from "./schema.graphql"
-import { url } from "inspector";
-
-type Link = {
-    id: string;
-    url: string;
-    description: string;
-}
-
-const links: Link[] = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
+import { Link, User } from "@prisma/client";
+import { GraphQLContext } from "./context";
+import { compare, hash } from "bcryptjs";
+import { APP_SECRET } from "./auth";
+import { sign } from "jsonwebtoken";
+import { error } from "console";
 
 const resolvers = {
     Query: {
-        info:() => 'This is the API of a HackerNews Clone',
-        feed:() => links,
+        info: () => 'This is the API of a HackerNews Clone',
+        feed: async (parent: unknown, args: {}, context: GraphQLContext) => {
+            return context.prisma.link.findMany();
+        },
+        me: (parent: unknown, args: {}, context: GraphQLContext) => {
+            if (context.currentUser === null) {
+                throw new Error("Unauthenticated")
+            }
+            return context.currentUser;
+        },
+        links: (parent: User, args: {}, context: GraphQLContext) => {
+            context.prisma.user.findUnique({ where: { id: parent.id } }).links()
+        }
     },
 
     Link: {
         id: (parent: Link) => parent.id,
         description: (parent: Link) => parent.description,
-        url: (parent: Link) => parent.url
+        url: (parent: Link) => parent.url,
+        postedBy: async (parent: Link, args: {}, context: GraphQLContext) => {
+            if (!parent.postedById) {
+                return null;
+            }
+
+            return context.prisma.link
+                .findUnique({ where: { id: parent.id } })
+                .postedBy();
+        },
     },
 
     Mutation: {
-    	post: (parent: unknown, args: {description: string, url: string}) => {
-		let idCount = links.length;
+        signup: async (parent: unknown, args: { email: string, password: string, name: string }, context: GraphQLContext
+        ) => {
+            const password = await hash(args.password, 10)
+            const user = await context.prisma.user.create({
+                data: { ...args, password }
+            })
 
-		const link: Link = {
-			id: `link-${idCount++}`,
-			description: args.description,
-			url: args.url,
-    };
-    links.push(link)
+            const token = sign({ userId: user.id }, APP_SECRET);
 
-    return link;
-	}
+            return {
+                token,
+                user,
+            }
+        },
+
+        login: async (parent: unknown, args: { email: string, password: string }, context: GraphQLContext) => {
+            const user = await context.prisma.user.findUnique({
+                where: { email: args.email },
+            })
+
+            if (!user) {
+                throw new Error("No such user found")
+            }
+
+            const valid = await compare(args.password, user.password);
+            if (!valid) {
+                throw new Error("Invalid Password")
+            }
+
+            const token = sign({ userId: user.id }, APP_SECRET)
+
+            return {
+                token,
+                user,
+            }
+        },
+
+        post: (parent: unknown, args: { description: string, url: string }, context: GraphQLContext) => {
+            if (context.currentUser === null) {
+                throw new Error("Unauthenticated");
+            }
+
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                    postedBy: { connect: { id: context.currentUser.id } }
+                },
+            });
+            return newLink
+        }
     }
-   
 }
 
 export const schema = makeExecutableSchema({
-	typeDefs,
-	resolvers,
+    typeDefs,
+    resolvers,
 });
