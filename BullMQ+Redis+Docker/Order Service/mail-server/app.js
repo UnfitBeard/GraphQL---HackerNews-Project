8405ac62aa818a01e0c1a9354d87e670f2f7cdca
Mr.Nobody <njunge995@gmail.com>
2025-06-11 09:20:23 +0300

    Howdy cron jobs: job scheduling

--- BullMQ+Redis+Docker/Order Service/mail-server/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Worker } from "bullmq";
-
-const app = express();
-app.use(express.json());
-
-app.listen(3002, () => {
-  console.log("User service started on port 3001");
-});
